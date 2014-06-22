1f5cbcfe874275806ba500a04d0a21c07254f7e9
mzivicnjak <mzivicnj@foi.hr>
2014-06-22 20:59:43 +0200

    Verzija 3
    Nastavak na verziju2

---
+++ Projektna dokumentacija/Baza_podataka/pogledPosudba.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Baza_podataka
+{
+    public class pogledPosudba
+    {
+        private int p_idPosudba;
+        private int p_Clan_idClan; // vanjski kljuc
+        private int p_Film_idFilm; // vanjski kljuc
+        private string p_ime;
+        private string p_prezime;
+        private string p_email;
+        private string p_telefon;
+        private string p_naziv; 
+        private string p_datum_posudbe;
+        private string p_posudjen_do;
+        private string p_datum_vracanja; 
+
+        /// <summary>
+        /// Konstruktor klase
+        /// </summary>
+        public pogledPosudba()
+        {
+            
+        }
+
+        /// <summary>
+        /// Puni objekt sa podacima iz odgovorajućih tablica
+        /// </summary>
+        /// <param name="dr">DataReader objekt sa podacima iz tablica</param>
+        public pogledPosudba(DbDataReader dr)
+        {
+            if (dr != null)
+            {
+                p_idPosudba = int.Parse(dr["idPosudba"].ToString());
+                p_Clan_idClan = int.Parse(dr["Clan_idClan"].ToString());
+                p_Film_idFilm = int.Parse(dr["Film_idFilm"].ToString());
+                p_datum_posudbe = dr["datum_posudbe"].ToString();
+                p_posudjen_do = dr["posudjen_do"].ToString();
+                p_datum_vracanja = dr["datum_vracanja"].ToString();
+                p_ime = dr["ime"].ToString();
+                p_prezime = dr["prezime"].ToString();
+                p_email = dr["email"].ToString();
+                p_telefon = dr["telefon"].ToString();
+                p_naziv = dr["naziv"].ToString();
+            }
+
+        }
+
+        /// <summary>
+        /// Jedinstveni identifikator posudbe
+        /// </summary>
+        public int idPosudba
+        {
+            get
+            {
+                return p_idPosudba;
+            }
+        }
+
+        /// <summary>
+        /// Jedinstveni identifikator clana
+        /// </summary>
+        public int Clan_idClan
+        {
+            get
+            {
+                return p_Clan_idClan;
+            }
+        }
+
+        /// <summary>
+        /// Jedinstveni identifikator filma
+        /// </summary>
+        public int Film_idFilm
+        {
+            get
+            {
+                return p_Film_idFilm;
+            }
+        }
+
+        /// <summary>
+        /// Ime osobe
+        /// </summary>
+        public string ime
+        {
+            get
+            {
+                return p_ime;
+            }
+        }
+
+        /// <summary>
+        /// Prezime osobe
+        /// </summary>
+        public string prezime
+        {
+            get
+            {
+                return p_prezime;
+            }
+        }
+
+        /// <summary>
+        /// Email osobe
+        /// </summary>
+        public string email
+        {
+            get
+            {
+                return p_email;
+            }
+        }
+
+        /// <summary>
+        /// Telefonski broj osobe
+        /// </summary>
+        public string telefon
+        {
+            get
+            {
+                return p_telefon;
+            }
+        }
+
+        /// <summary>
+        /// Naziv filma
+        /// </summary>
+        public string naziv
+        {
+            get
+            {
+                return p_naziv;
+            }
+        }
+
+        /// <summary>
+        /// Datum posudbe filma
+        /// </summary>
+        public string datum_posudbe
+        {
+            get
+            {
+                return p_datum_posudbe;
+            }
+        }
+
+        /// <summary>
+        /// Datum isteka posudbe filma
+        /// </summary>
+        public string posudjen_do
+        {
+            get
+            {
+                return p_posudjen_do;
+            }
+        }
+
+        /// <summary>
+        /// Datum vracanja posudjenog filma
+        /// </summary>
+        public string datum_vracanja
+        {
+            get
+            {
+                return p_datum_vracanja;
+            }
+        }
+
+        /// <summary>
+        /// Dohvaća sve posudbe i dohvaća sve potrebne podatke iz ostalih tablica i vraća ih u obliku generičke liste
+        /// </summary>
+        /// <param name="ne_vracene_samo">Dohvat filmova koji nisu vraćeni ako je true</param>
+        /// <returns>Lista svih posudbi prosirena s atributima ostalih tablica s kojima je vezana</returns>
+        public static List<pogledPosudba> DohvatiSvePosudbe(string dodatak="")
+        {
+            List<pogledPosudba> lista = new List<pogledPosudba>();
+            string sqlUpit = "select idPosudba,p.Clan_idClan,p.Film_idFilm,ime,prezime,email,telefon,datum_posudbe,posudjen_do,datum_vracanja,f.naziv from Posudba p LEFT OUTER JOIN Clan c ON p.Clan_idClan = c.idClan LEFT OUTER JOIN Osoba o ON o.idOsoba = c.idOsoba LEFT OUTER JOIN Film f ON f.idFilm = p.Film_idFilm " + dodatak + "Group by 1;";
+            DbDataReader dr = Baza_podataka.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                pogledPosudba pos = new pogledPosudba(dr);
+                lista.Add(pos);
+            }
+            dr.Close();
+            return lista;
+        }
+
+        /// <summary>
+        /// Dohvaća sve posudbe i dohvaća sve potrebne podatke iz ostalih tablica i vraća ih u obliku generičke liste
+        /// </summary>
+        /// <param name="trazeniID">Jedinstveni identifikator clana prosljedjen kao string</param>
+        /// <param name="ne_vracene_samo">Dohvat filmova koji nisu vraćeni ako je true</param>
+        /// <returns>Lista svih posudbi nekog clana prosirena s atributima ostalih tablica s kojima je vezana</returns>
+        public static List<pogledPosudba> DohvatiSvePosudbePremaClanu(string trazeniID, bool ne_vracene_samo = false)
+        {
+            List<pogledPosudba> lista = new List<pogledPosudba>();
+            string dodatak="";
+            if (ne_vracene_samo) dodatak = " and datum_vracanja=''";
+            string sqlUpit = "select idPosudba,p.Clan_idClan,p.Film_idFilm,ime,prezime,email,telefon,datum_posudbe,posudjen_do,datum_vracanja,f.naziv from Posudba p LEFT OUTER JOIN Clan c ON p.Clan_idClan = c.idClan LEFT OUTER JOIN Osoba o ON o.idOsoba = c.idOsoba LEFT OUTER JOIN Film f ON f.idFilm = p.Film_idFilm where p.Clan_idClan = " + trazeniID + dodatak + " Group by 1;";
+            DbDataReader dr = Baza_podataka.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                pogledPosudba pos = new pogledPosudba(dr);
+                lista.Add(pos);
+            }
+            dr.Close();
+            return lista;
+        }
+
+        /// <summary>
+        /// Dohvaća sve posudbe i dohvaća sve potrebne podatke iz ostalih tablica i vraća ih u obliku generičke liste
+        /// </summary>
+        /// <param name="trazeniID">Jedinstveni identifikator filma prosljedjen kao string</param>
+        /// <param name="ne_vracene_samo">Dohvat filmova koji nisu vraćeni ako je true</param>
+        /// <returns>Lista svih posudbi nekog filma prosirena s atributima ostalih tablica s kojima je vezana</returns>
+        public static List<pogledPosudba> DohvatiSvePosudbePremaFilmu(string trazeniID, bool ne_vracene_samo = false)
+        {
+            List<pogledPosudba> lista = new List<pogledPosudba>();
+            string dodatak = "";
+            if (ne_vracene_samo) dodatak = " and datum_vracanja=''";
+            string sqlUpit = "select idPosudba,p.Clan_idClan,p.Film_idFilm,ime,prezime,email,telefon,datum_posudbe,posudjen_do,datum_vracanja,f.naziv from Posudba p LEFT OUTER JOIN Clan c ON p.Clan_idClan = c.idClan LEFT OUTER JOIN Osoba o ON o.idOsoba = c.idOsoba LEFT OUTER JOIN Film f ON f.idFilm = p.Film_idFilm where p.Film_idFilm = " + trazeniID + dodatak + " Group by 1;";
+            DbDataReader dr = Baza_podataka.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                pogledPosudba pos = new pogledPosudba(dr);
+                lista.Add(pos);
+            }
+            dr.Close();
+            return lista;
+        }
+
+    }
+}
---
+++ Projektna dokumentacija/Baza_podataka/pogledZaposlenici.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Baza_podataka
+{
+    public class pogledZaposlenici
+    {
+        private int p_idZaposlenik;
+        private string p_datum_zaposljenja;
+        private int p_idOsoba;
+        private string p_ime;
+        private string p_prezime;
+        private string p_email;
+        private string p_telefon;
+
+
+        /// <summary>
+        /// Konstruktor klase
+        /// </summary>
+        public pogledZaposlenici()
+        {
+            
+        }
+
+        /// <summary>
+        /// Puni objekt sa podacima iz tablice Zaposlenik i Osoba
+        /// </summary>
+        /// <param name="dr">DataReader objekt sa podacima iz tablice Zaposlenik povezanih s Osobom</param>
+        public pogledZaposlenici(DbDataReader dr)
+        {
+            if (dr != null)
+            {
+                p_idOsoba = int.Parse(dr["idOsoba"].ToString());
+                p_idZaposlenik = int.Parse(dr["idZaposlenik"].ToString());
+                p_datum_zaposljenja = dr["datum_zaposljenja"].ToString();
+                p_ime = dr["ime"].ToString();
+                p_prezime = dr["prezime"].ToString();
+                p_email = dr["email"].ToString();
+                p_telefon = dr["telefon"].ToString();
+            }
+
+        }
+
+        /// <summary>
+        /// Jedinstveni identifikator zaposlenika
+        /// </summary>
+        public int idZaposlenik
+        {
+            get
+            {
+                return p_idZaposlenik;
+            }
+        }
+
+        /// <summary>
+        /// Jedinstveni identifikator osobe
+        /// </summary>
+        public int idOsoba
+        {
+            get
+            {
+                return p_idOsoba;
+            }
+        }
+
+        /// <summary>
+        /// Ime osobe
+        /// </summary>
+        public string ime
+        {
+            get
+            {
+                return p_ime;
+            }
+        }
+
+        /// <summary>
+        /// Prezime osobe
+        /// </summary>
+        public string prezime
+        {
+            get
+            {
+                return p_prezime;
+            }
+        }
+
+        /// <summary>
+        /// Email osobe
+        /// </summary>
+        public string email
+        {
+            get
+            {
+                return p_email;
+            }
+        }
+
+        /// <summary>
+        /// Telefonski broj osobe
+        /// </summary>
+        public string telefon
+        {
+            get
+            {
+                return p_telefon;
+            }
+        }
+
+        /// <summary>
+        /// Datum zaposljavanja zaposenika
+        /// </summary>
+        public string datum_zaposljenja
+        {
+            get
+            {
+                return p_datum_zaposljenja;
+            }
+        }
+
+        /// <summary>
+        /// Dohvaća sve zaposlenike i osobe iz baze i vraća ih u obliku generičke liste
+        /// </summary>
+        /// <returns>Lista zaposlenika prosirena s atributima tablice osoba s kojom je vezana</returns>
+        public static List<pogledZaposlenici> DohvatiZaposlenike()
+        {
+            List<pogledZaposlenici> lista = new List<pogledZaposlenici>();
+            string sqlUpit = "select idZaposlenik,z.idOsoba,ime,prezime,email,telefon,datum_zaposljenja from Zaposlenik z LEFT OUTER JOIN Osoba o ON o.idOsoba = z.idOsoba Group by 1;";
+            DbDataReader dr = Baza_podataka.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                pogledZaposlenici zap = new pogledZaposlenici(dr);
+                lista.Add(zap);
+            }
+            dr.Close();
+            return lista;
+        }
+
+        /// <summary>
+        /// Metoda koja nadjačava ToString metodu
+        /// </summary>
+        /// <returns>Ime i prezime osobe</returns>
+        public override string ToString()
+        {
+            return ime + " " + prezime;
+        }
+
+
+    }
+}
